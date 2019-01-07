5a42704048c140d43dd8956c67502588a0619d6e
mosoire <mosoire@outlook.com>
2019-01-07 21:09:22 +0800

    bugs to fix

--- ml2018winter_hw4/pca/hack_pca.py
+++ ml2018winter_hw4/pca/hack_pca.py
@@ -1,26 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pca import PCA
+from PIL import Image
 
 def hack_pca(filename):
     '''
     Input: filename -- input image file name/path
     Output: img -- image without rotation
     '''
-    img_r = (plt.imread(filename)).astype(np.float64)
-
+    #img_r = (plt.imread(filename)).astype(np.float64)
     # YOUR CODE HERE
     # begin answer
-    R, G, B, A = img_r[:,:,0], img_r[:,:,1], img_r[:,:,2], img_r[:,:,3]
-    Red = ((1 - A) * R) + (A * R)
-    Green = ((1 - A) * G) + (A * G)
-    Blue = ((1 - A) * B) + (A * B)
-    gray = 0.2989 * Red + 0.5870 * Green + 0.1140 * Blue
-
-    eigval, eigvec = PCA(gray)
-
-    gray_avg = np.average(gray, axis=0)
-    d = gray - gray_avg
-    r = np.matmul(d, eigvec) + gray_avg
-    return r.astype(np.float64)
+    img_r = Image.open(filename).convert('L')
+    img_r = np.array(img_r, dtype=np.float64)
+    print(img_r.shape[0] * img_r.shape[1])
+    print(np.sum(img_r ==0))
+    p = img_r.shape[1]
+    eigval, eigvec = PCA(img_r)
+    base = np.matmul(img_r, eigvec).astype(np.int64)
+    
+    return img_r[base]
     # end answer
--- ml2018winter_hw4/pca/pca.py
+++ ml2018winter_hw4/pca/pca.py
@@ -16,17 +16,14 @@ def PCA(data):
     # YOUR CODE HERE
     # Hint: you may need to normalize the data before applying PCA
     # begin answer
-    data_avg = np.average(data, axis=0)
-    #data_std = np.std(data, axis=0)
-    #d = (data - data_avg) / data_std
-    d = data - data_avg
-    cov_data = np.cov(d.T)
-    eigval, eigvec = np.linalg.eig(cov_data)
+    N = data.shape[0]
+    data = data - np.mean(data, axis=0)
+    S = np.matmul(data.T, data) / N
+    eigval, eigvec = np.linalg.eig(S)
 
     sorted_eig_idx = np.argsort(-eigval)
     eigvalue = eigval[sorted_eig_idx]
     eigvector = eigvec[:, sorted_eig_idx]
 
-    print(eigvector.shape)
     return eigvalue, eigvector
     # end answer
