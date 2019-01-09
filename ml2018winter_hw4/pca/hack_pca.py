b909d427e4144da73d36d2b1ef757b82fbad98fc
mosoire <mosoire@outlook.com>
2019-01-09 13:12:38 +0800

    complete hw4

--- ml2018winter_hw4/pca/hack_pca.py
+++ ml2018winter_hw4/pca/hack_pca.py
@@ -1,30 +1,24 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy import misc
 from pca import PCA
 from PIL import Image
+from math import acos
 
 def hack_pca(filename):
     '''
     Input: filename -- input image file name/path
     Output: img -- image without rotation
     '''
-    #img_r = (plt.imread(filename)).astype(np.float64)
     # YOUR CODE HERE
     # begin answer
     img_r = Image.open(filename).convert('L')
     img_r = np.array(img_r, dtype=np.float64)
 
-    coord = np.where(((img_r > 0) & (img_r < 150)))
+    coord = np.where(((img_r > 0) & (img_r < 100)))
     coord = np.array(coord).T
     
     eigval, eigvec = PCA(coord)
+    angle = acos(eigvec[0,0] / np.linalg.norm(eigvec[:,0])) / np.pi * 180 - 90
 
-    new_cood = np.matmul(eigvec.T, coord.T) + 50
-    new_cood = np.array(new_cood, dtype=np.int64)
-
-    x = tuple(new_cood)
-    
-    img = np.zeros((img_r.shape[0]*3, img_r.shape[1]))
-    img[x] = 50
-    return img
+    return misc.imrotate(img_r, angle)
     # end answer
